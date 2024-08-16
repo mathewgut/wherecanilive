c181344d944f4cb7045b91968634147c11018e40
mathewgut <mathewgutoskie@gmail.com>
2024-08-16 17:37:02 -0400

    added some city geojson, created listener for clicking and focusing on cities in regions

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let map;
 
-// default position
+// bool for triggering event listeners on window
+let activeWindow = false; 
+
+// default position`
 const canadaDefault = { lat: 56.1304, lng: -106.3468 };
 
 // allows functions to know which region is focused on
@@ -12,7 +15,7 @@ let regionZoom = false;
 // rinky dinky and super inefficent db
 const regions = {
   bColumbia: {coord: [54.324821, -124.861467], oneBed: 'value', twoBed: 2902, color:'value', title: 'British Columbia', window : {},
-    cities: {vancouver: {coord: [49.259500, -123.106539], oneBed: 2761, twoBed: 3666}, burnaby: {coord: [49.233502, -122.985689], oneBed: 2566, twoBed: 3184}}},
+    cities: {vancouver: {coord: [49.259500, -123.106539], boundry: './maps/vancouver.geojson' , oneBed: 2761, twoBed: 3666}, burnaby: {coord: [49.233502, -122.985689], oneBed: 2566, twoBed: 3184}}},
   alberta: {coord: [55.648427, -115.083635], oneBed: 'value', twoBed: 1986, color:'value', title: 'Alberta', window : {},
     cities: {calgary: {coord: [51.038020, -114.073305], oneBed: 1751, twoBed: 2157}, edmonton: {coord: [53.538191, -113.497395], oneBed: 1389, twoBed: 1789}}},
   sask: {coord: [54.503829, -105.986955], oneBed: 'value', twoBed: 1432, color:'value', title: 'Saskatchewan', window : {},
@@ -20,9 +23,9 @@ const regions = {
   manitoba: {coord: [55.636027, -97.022111], oneBed: 'value', twoBed: 1781, color:'value', title: 'Manitoba', window : {},
     cities: {winnipeg: {coord: [49.887948, -97.138026], oneBed: 1442, twoBed: 1799}}},
   ontario: {coord: [51.269352, -86.514159], oneBed: 'value', twoBed: 2638, color:'value', title: 'Ontario', window : {},
-    cities: {toronto: {coord: [43.710820, -79.394462], oneBed: 2443, twoBed: 3198}, mississauga: {coord: [43.595824, -79.652415], oneBed: 2364, twoBed: 2764}}},
+    cities: {toronto: {coord: [43.710820, -79.394462], boundry: './maps/toronto.geojson', oneBed: 2443, twoBed: 3198}, mississauga: {coord: [43.595824, -79.652415], oneBed: 2364, twoBed: 2764}}},
   quebec: {coord: [51.120750, -72.991588], oneBed: 'value', twoBed: 2159, color:'value', title: 'Quebec', window : {},
-    cities: {montreal: {coord:[45.521425, -73.619292], oneBed: 1756, twoBed: 2295}, gatineau: {coord:[45.451650, -75.698136], oneBed: 1736, twoBed: 1937}}},
+    cities: {montreal: {coord:[45.521425, -73.619292], boundry: './maps/montreal.geojson',oneBed: 1756, twoBed: 2295}, gatineau: {coord:[45.451650, -75.698136], oneBed: 1736, twoBed: 1937}}},
   nScotia: {coord: [45.0778, -63.5467], oneBed: 'value', twoBed: 2670, color:'value', title: 'Nova Scotia', window : {},
     cities: {halifax: {coord: [44.649121, -63.591530], oneBed: 2050, twoBed: 2506}}},
 };
@@ -41,7 +44,6 @@ const createRegionNamesArray = () => {
 // then it can be accessed to be closed. needs reference to close
 function dropMarker (position, content='lol'){
   let coord = { lat: position[0], lng: position[1] }
-  console.log(coord)
   const regionInfoWindow = new google.maps.InfoWindow();
   regionInfoWindow.setPosition(coord);
   regionInfoWindow.open(map);
@@ -49,7 +51,7 @@ function dropMarker (position, content='lol'){
   return regionInfoWindow
 }
 
-function createWindowContent(region){
+function createRegionWindowContent(region){
   const windowContainer = document.createElement('div');
   windowContainer.setAttribute('class','window-container');
   
@@ -83,8 +85,9 @@ function createWindowContent(region){
     map.setOptions({gestureHandling: 'auto'})
     map.setCenter(canadaDefault);
     map.setZoom(4);
-    regionZoom = true;
+    regionZoom = false;
     region.window.close()
+    activeWindow = false;
   })
 
   areaDetailsContainer.appendChild(areaCities);
@@ -92,6 +95,7 @@ function createWindowContent(region){
   windowContainer.appendChild(areaDetailsContainer);
   windowContainer.appendChild(backButton);
 
+  activeWindow = true;
   return windowContainer
 }
 
@@ -104,32 +108,22 @@ function regionFocus(name){
       map.setCenter({lat: item[1].coord[0], lng: item[1].coord[1]});
       // using unupdated focusedRegion value to close last region window
       regionObjects.forEach((item)=>{
-        item[1].title == focusedRegion ? item[1].window.close() : false; 
+        if (item[1].title == focusedRegion){
+          item[1].window.close() 
+          activeWindow = false
+        }
       })
       focusedRegion = item[1].title;
       regionZoom = true;
       map.setOptions({gestureHandling: 'none'})
-      const windowContainer = createWindowContent(item[1])
+      const windowContainer = createRegionWindowContent(item[1])
 
       const infoMarker = dropMarker(item[1].coord, windowContainer);
 
       item[1].window = infoMarker;
-      console.log(`${name} window: `,item[1].window)
-
-      // this will suffer from a recursive loop. region focus needs to be able to be called on this
-      // move this to another function that relates to the current region
-      windowContainer.addEventListener('click',(event) => {
-        console.log(event.target.id)
-        if(event.target.id.search('city') != -1){
-          const cityKey = event.target.textContent.toLowerCase();
-          Object.entries(item[1].cities).forEach((city,pos)=>{
-            city[0] == cityKey ? map.setCenter({lat: city[1].coord[0], lng: city[1].coord[1]}) : false;
-            
-          })
-        }
-      })
     }
   })
+
 }
 
 // basic sleep helper function
@@ -193,6 +187,7 @@ async function initMap() {
   map.data.setStyle({
     clickable: true,
     fillOpacity: 0,
+    
   });
 
   // set region line opacity to lower when zoomed,
@@ -202,6 +197,7 @@ async function initMap() {
         map.data.setStyle(() => {
           return {
             strokeOpacity: zoomLevel < 10 ? 0.4 : 0.7,
+            
           };
        });
   });
@@ -253,11 +249,60 @@ async function initMap() {
     const selection = event.feature.getProperty("name")
     Object.entries(regions).forEach((item, index)=>{
       const cityCost = ` City: ${Object.keys(item[1].cities)[0]} onebed: ${Object.values(item[1].cities)[0].oneBed} two bed: ${Object.values(item[1].cities)[0].twoBed}`
-      console.log('title: ',item[1].title,  currentRegion.textContent, 'info: ', cityCost)
       item[1].title == selection ? currentRegionInfo.textContent = cityCost: currentRegion.textcontent = 'Canada';})
       regionFocus(selection);
       currentRegion.textContent = `Current region: ${selection}`;
   })
+
+  // city targeting if name clicked
+  const mapDiv = document.getElementById('map');
+  mapDiv.addEventListener('click',(event)=>{
+    if (activeWindow){
+      if(event.target.id.search('city') != -1){
+        console.log(focusedRegion)
+        const cityKey = event.target.textContent.toLowerCase();
+        let currRegion = Object;
+        Object.entries(regions).forEach((item,index) => {
+          if(item[1].title == focusedRegion){
+            currRegion = item[1];
+          }
+        })
+        console.log(currRegion)
+        Object.entries(currRegion.cities).forEach((city)=>{
+          if (city[0] == cityKey){
+            map.setCenter({lat: city[1].coord[0], lng: city[1].coord[1]})
+            map.setZoom(11);
+            map.data.setStyle({
+              clickable: true,
+              fillOpacity: 0,
+              strokeOpacity: 0,
+              clickable: false,
+            });
+           
+
+            const cityDataLayer = new google.maps.Data();
+            cityDataLayer.loadGeoJson(city[1].boundry);
+            cityDataLayer.setMap(map);
+            cityDataLayer.setStyle({
+              fillOpacity: 0, 
+              strokeOpacity: 0.25,
+              visible: true,
+            })
+            
+            map.setOptions({gestureHandling: 'auto'})
+            
+            // this is just for testing so i can get back to country map
+            // replace with button on city info window
+            cityDataLayer.addListener('click',event =>{
+              cityDataLayer.setMap(null)
+              regionFocus(focusedRegion);
+            })
+          }
+        })
+      }
+  };
+})
+
   
   // supporting element creation
   const areaInfo = document.createElement('div');
@@ -298,6 +343,10 @@ async function initMap() {
     console.log('right arrow click, current position: ', currentPosition);
     currentPosition == names.length-1 ? regionFocus(names[0]) : regionFocus(names[currentPosition+1])
   })
+
+ 
+  
+  
 
   // legacy info, to be factored out
   areaInfo.appendChild(currentRegion);
