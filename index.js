c9b2a806777004dc3641fff5f1172ffe122d0c7b
mathewgut <mathewgutoskie@gmail.com>
2024-08-16 18:30:37 -0400

    added capital cities for each province

--- index.js
+++ index.js
@@ -17,17 +17,17 @@ const regions = {
   bColumbia: {coord: [54.324821, -124.861467], oneBed: 'value', twoBed: 2902, color:'value', title: 'British Columbia', window : {},
     cities: {vancouver: {coord: [49.259500, -123.106539], boundry: './maps/vancouver.geojson' , oneBed: 2761, twoBed: 3666}, burnaby: {coord: [49.233502, -122.985689], oneBed: 2566, twoBed: 3184}}},
   alberta: {coord: [55.648427, -115.083635], oneBed: 'value', twoBed: 1986, color:'value', title: 'Alberta', window : {},
-    cities: {calgary: {coord: [51.038020, -114.073305], oneBed: 1751, twoBed: 2157}, edmonton: {coord: [53.538191, -113.497395], oneBed: 1389, twoBed: 1789}}},
+    cities: {calgary: {coord: [51.038020, -114.073305], boundry: './maps/calgary.geojson',oneBed: 1751, twoBed: 2157}, edmonton: {coord: [53.538191, -113.497395], oneBed: 1389, twoBed: 1789}}},
   sask: {coord: [54.503829, -105.986955], oneBed: 'value', twoBed: 1432, color:'value', title: 'Saskatchewan', window : {},
-    cities: {regina: {coord: [50.450855, -104.618047], oneBed: 1334, twoBed: 1541}, saskatoon: {coord: [52.134032, -106.646741]}}},
+    cities: {regina: {coord: [50.450855, -104.618047], boundry: './maps/regina.geojson',oneBed: 1334, twoBed: 1541}, saskatoon: {coord: [52.134032, -106.646741]}}},
   manitoba: {coord: [55.636027, -97.022111], oneBed: 'value', twoBed: 1781, color:'value', title: 'Manitoba', window : {},
-    cities: {winnipeg: {coord: [49.887948, -97.138026], oneBed: 1442, twoBed: 1799}}},
+    cities: {winnipeg: {coord: [49.887948, -97.138026], boundry: './maps/winnipeg.geojson',oneBed: 1442, twoBed: 1799}}},
   ontario: {coord: [51.269352, -86.514159], oneBed: 'value', twoBed: 2638, color:'value', title: 'Ontario', window : {},
     cities: {toronto: {coord: [43.710820, -79.394462], boundry: './maps/toronto.geojson', oneBed: 2443, twoBed: 3198}, mississauga: {coord: [43.595824, -79.652415], oneBed: 2364, twoBed: 2764}}},
   quebec: {coord: [51.120750, -72.991588], oneBed: 'value', twoBed: 2159, color:'value', title: 'Quebec', window : {},
     cities: {montreal: {coord:[45.521425, -73.619292], boundry: './maps/montreal.geojson',oneBed: 1756, twoBed: 2295}, gatineau: {coord:[45.451650, -75.698136], oneBed: 1736, twoBed: 1937}}},
   nScotia: {coord: [45.0778, -63.5467], oneBed: 'value', twoBed: 2670, color:'value', title: 'Nova Scotia', window : {},
-    cities: {halifax: {coord: [44.649121, -63.591530], oneBed: 2050, twoBed: 2506}}},
+    cities: {halifax: {coord: [44.649121, -63.591530], boundry: './maps/halifax.geojson' ,oneBed: 2050, twoBed: 2506}}},
 };
 
 // region name list to search
@@ -101,7 +101,7 @@ function createRegionWindowContent(region){
 
 // sets default content and parameters for a specified region
 function regionFocus(name){
-  let regionObjects = Object.entries(regions);
+  const regionObjects = Object.entries(regions);
   regionObjects.forEach((item) => {
     if(item[1].title == name){
       map.setZoom(6)
@@ -279,7 +279,8 @@ async function initMap() {
               clickable: false,
             });
            
-
+            // create new data layer so styles can be edited seperately
+            // from region data layer
             const cityDataLayer = new google.maps.Data();
             cityDataLayer.loadGeoJson(city[1].boundry);
             cityDataLayer.setMap(map);
